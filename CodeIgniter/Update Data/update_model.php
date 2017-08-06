68db6a87cda8b6af623a812ca080f1629b5bc6d6
KimPP <kimpastor@web.de>
2017-08-06 22:14:26 +0200

    CodeIgniter Program Files

---
+++ CodeIgniter/Update Data/update_model.php
@@ -0,0 +1,29 @@
+<?php
+
+class update_model extends CI_Model{
+
+function show_students(){
+$query = $this->db->get('1stimme');
+$query_result = $query->result();
+return $query_result;
+}
+
+
+
+function show_student_id($data){
+        $this->db->select('*');
+        $this->db->from('1stimme');
+        $this->db->where('W_ID', $data);
+        $query = $this->db->get();
+        $result = $query->result();
+        return $result;  
+    }
+
+function update_student_id1($id,$data){
+     $this->db->where('W_ID', $id);
+     $this->db->update('1stimme', $data);  
+    }
+ 
+}
+
+?>
