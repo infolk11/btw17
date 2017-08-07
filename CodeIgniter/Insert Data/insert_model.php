e11b95dc8b4e0a376930aad1ad9d60327f23b06e
KimPP <kimpastor@web.de>
2017-08-08 00:17:01 +0200

    Add files via upload

---
+++ CodeIgniter/Insert Data/insert_model.php
@@ -0,0 +1,11 @@
+<?php
+class insert_model extends CI_Model{
+function __construct() {
+parent::__construct();
+}
+function form_insert($data){
+// Inserting in Table(1stimme) of Database(wahl_17)
+$this->db->insert('1stimme', $data);
+}
+}
+?>
