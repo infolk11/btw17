c3f3265ea8a08afcf2ce589b3f561cc9e2dd7f70
KimPP <kimpastor@web.de>
2017-08-08 01:39:15 +0200

    Add files via upload

---
+++ CodeIgniter/Insert Data/Wahllokal/wahl_insert_ctrl.php
@@ -0,0 +1,39 @@
+<?php
+
+class wahl_insert_ctrl extends CI_Controller {
+
+function __construct() {
+parent::__construct();
+$this->load->model('wahl_insert_model');
+}
+
+
+function index() {
+
+//Setting values for tabel columns
+$data = array(
+'W_ID' => $this->input->post('dw_id'),
+'W_Bezeichnung' => $this->input->post('dbez'),
+'PLZ' => $this->input->post('dplz'),
+'Straße' => $this->input->post('dstr'),
+'Wahlberechtigte' => $this->input->post('dber'),
+'Wahlbeteiligung' => $this->input->post('dbet'),
+);
+//Transfering data to Model
+$this->wahl_insert_model->form_insert($data);
+$data['message'] = 'Daten erfolgreich eingegeben!';
+//Loading View
+$this->load->view('wahl_insert_view', $data);
+}
+
+function show_view() {
+
+//Loading View
+$this->load->view('wahl_insert_view');
+}
+
+}
+
+
+
+?>
---
+++ CodeIgniter/Insert Data/Wahllokal/wahl_insert_view.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <title>Insert</title>
+        <link href='http://fonts.googleapis.com/css?family=Marcellus' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(). "css/styles.css" ?>">
+    </head> 
+<body>
+        <div id="container">
+            <div id="wrapper">
+				<h1>Erststimme</h1><hr/>
+				<?php if (isset($message)) { ?>
+				<CENTER><h3 style="color:green;">Daten erfolgreich eingegeben!</h3></CENTER><br>
+				<?php } ?>
+
+				<?php echo form_open('wahl_insert_ctrl'); ?>
+				<?php echo form_label('W_ID :'); ?> <?php echo form_error('dw_id'); ?><br/>
+				<?php echo form_input(array('name' => 'dw_id')); ?><br/>
+
+				<?php echo form_label('W_Bezeichnung :'); ?> <?php echo form_error('dbez'); ?><br/>
+				<?php echo form_input(array('name' => 'dbez')); ?><br/>
+
+				<?php echo form_label('PLZ :'); ?> <?php echo form_error('dplz'); ?><br/>
+				<?php echo form_input(array('name' => 'dplz')); ?><br/>
+				
+				<?php echo form_label('Straße :'); ?> <?php echo form_error('dstr'); ?><br/>
+				<?php echo form_input(array('name' => 'dstr')); ?><br/>
+				
+				<?php echo form_label('Wahlberechtigte :'); ?> <?php echo form_error('dber'); ?><br/>
+				<?php echo form_input(array('name' => 'dber')); ?><br/>
+				
+				<?php echo form_label('Wahlbeteiligung :'); ?> <?php echo form_error('dbet'); ?><br/>
+				<?php echo form_input(array('name' => 'dbet')); ?><br/>
+
+				<?php echo form_submit(array('value' => 'Anlegen')); ?>
+				<?php echo form_close(); ?><br/>
+
+            </div> 
+        </div>
+
+</body>
+</html>
