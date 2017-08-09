a2d3c8383e68adb3669ca7d860a411ad3421161b
AresCrew <Felix.schroeter@gmx.net>
2017-08-09 21:44:28 +0200

    Update navi_view.php
    Formatierung und Sorteirung der Navigation

--- CodeIgniter/navi_view.php
+++ CodeIgniter/navi_view.php
@@ -15,40 +15,40 @@
 					<a href="<?php echo base_url() . "index.php/update_ctrl/show_student_id/-1/-1" ?>">Datensatz aktualisieren</a><br/>
 					<a href="<?php echo base_url() . "index.php/delete_ctrl/show_student_id/-1/-1" ?>">Datensatz löschen</a><br/>
                 </div>
-                <div id="wahllokal">
-					<p>Wahllokal</p>
-					<a href="<?php echo base_url() . "index.php/wahl_insert_ctrl/schow_view/-1/-1" ?>">Datensatz einfügen</a><br/>
-					<a href="<?php echo base_url() . "index.php/wahl_update_ctrl/show_student_id/-1/-1" ?>">Datensatz aktualisieren</a><br/>
-					<a href="<?php echo base_url() . "index.php/wahl_delete_ctrl/show_student_id/-1/-1" ?>">Datensatz löschen</a><br/>
-                </div> 
-				<div id="zweitstimme">
+		<div id="zweitstimme">
 					<p>Zweitstimme</p>
 					<a href="<?php echo base_url() . "index.php/zwei_insert_ctrl/show_view/-1/-1" ?>">Datensatz einfügen</a><br/>
 					<a href="<?php echo base_url() . "index.php/zwei_update_ctrl/show_student_id/-1/-1" ?>">Datensatz aktualisieren</a><br/>
 					<a href="<?php echo base_url() . "index.php/zwei_delete_ctrl/show_student_id/-1/-1" ?>">Datensatz löschen</a><br/>
                 </div> 
-		    		<div id="partei">
+		<div id="partei">
 					<p>Partei</p>
 					<a href="<?php echo base_url() . "index.php/part_insert_ctrl/show_view/-1/-1" ?>">Datensatz einfügen</a><br/>
 					<a href="<?php echo base_url() . "index.php/part_update_ctrl/show_student_id/-1/-1" ?>">Datensatz aktualisieren</a><br/>
 					<a href="<?php echo base_url() . "index.php/part_delete_ctrl/show_student_id/-1/-1" ?>">Datensatz löschen</a><br/>
 					<div id="partei">
                 </div> 
-				<div id="direktkandidaten">
+		<div id="direktkandidaten">
 					<p>Direktkandidaten</p>
 					<a href="<?php echo base_url() . "index.php/dkan_insert_ctrl/show_view/-1/-1" ?>">Datensatz einfügen</a><br/>
 					<a href="<?php echo base_url() . "index.php/dkan_update_ctrl/show_student_id/-1/-1" ?>">Datensatz aktualisieren</a><br/>
 					<a href="<?php echo base_url() . "index.php/dkan_delete_ctrl/show_student_id/-1/-1" ?>">Datensatz löschen</a><br/>
 					<div id="dkan">
-		         </div> 
-					<div id="stellt">
+		</div> 
+		<div id="stellt">
 					<p>Stellt</p>
 					<a href="<?php echo base_url() . "index.php/stellt_insert_ctrl/show_view/-1/-1" ?>">Datensatz einfügen</a><br/>
 					<a href="<?php echo base_url() . "index.php/stellt_update_ctrl/show_student_id/-1/-1" ?>">Datensatz aktualisieren</a><br/>
 					<a href="<?php echo base_url() . "index.php/stellt_delete_ctrl/show_student_id/-1/-1" ?>">Datensatz löschen</a><br/>
 					<div id="stellt">
 					
-                </div> 
+                </div>
+		<div id="wahllokal">
+					<p>Wahllokal</p>
+					<a href="<?php echo base_url() . "index.php/wahl_insert_ctrl/schow_view/-1/-1" ?>">Datensatz einfügen</a><br/>
+					<a href="<?php echo base_url() . "index.php/wahl_update_ctrl/show_student_id/-1/-1" ?>">Datensatz aktualisieren</a><br/>
+					<a href="<?php echo base_url() . "index.php/wahl_delete_ctrl/show_student_id/-1/-1" ?>">Datensatz löschen</a><br/>
+               </div> 	
             </div> 
         </div>
     </body>
