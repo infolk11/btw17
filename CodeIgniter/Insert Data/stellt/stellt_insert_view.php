072a934e029b3c56f4e8de574f71812a262bc702
KimPP <kimpastor@web.de>
2017-08-13 17:17:16 +0200

    Update stellt_insert_view.php

--- CodeIgniter/Insert Data/stellt/stellt_insert_view.php
+++ CodeIgniter/Insert Data/stellt/stellt_insert_view.php
@@ -20,7 +20,7 @@
 				<?php echo form_input(array('name' => 'dd_id')); ?><br/>
 
 				<?php echo form_submit(array('value' => 'Anlegen')); ?>
-				<p><a href="<?php echo base_url() . "index.php/navi_ctrl/load_navi_view"; ?>">Zurück zur Navigation</a></p>
+		   <p><a href="<?php echo base_url() . "index.php/navi_ctrl/load_navi_view"; ?>">Zurück zur Navigation</a></p>
 				<?php echo form_close(); ?><br/>
 				
             </div> 
