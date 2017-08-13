9b16717a4e04d78db40d62a1eff1b68cbdc7894f
KimPP <kimpastor@web.de>
2017-08-13 17:16:21 +0200

    Update erst_insert_view.php

--- CodeIgniter/Insert Data/1Stimme/erst_insert_view.php
+++ CodeIgniter/Insert Data/1Stimme/erst_insert_view.php
@@ -23,6 +23,7 @@
 				<?php echo form_input(array('name' => 'danzahl')); ?><br/>
 
 				<?php echo form_submit(array('value' => 'Anlegen')); ?>
+		   <p><a href="<?php echo base_url() . "index.php/navi_ctrl/load_navi_view"; ?>">Zurück zur Navigation</a></p>
 				<?php echo form_close(); ?><br/>
 
             </div> 
