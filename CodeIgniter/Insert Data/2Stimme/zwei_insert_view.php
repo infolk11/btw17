254976a92dc0f94630d56b9d48cf400f7f370088
KimPP <kimpastor@web.de>
2017-08-13 17:19:04 +0200

    Update zwei_insert_view.php

--- CodeIgniter/Insert Data/2Stimme/zwei_insert_view.php
+++ CodeIgniter/Insert Data/2Stimme/zwei_insert_view.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>Insert</title>
+        <title>zwei_insert</title>
         <link href='http://fonts.googleapis.com/css?family=Marcellus' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(). "css/styles.css" ?>">
     </head> 
