51a6b08d5c96747d2d5ea842089cdaa44bcf84f1
KimPP <kimpastor@web.de>
2017-08-13 17:19:45 +0200

    Add files via upload

--- CodeIgniter/Update Data/2Stimme/zwei_update_view.php
+++ CodeIgniter/Update Data/2Stimme/zwei_update_view.php
@@ -12,10 +12,10 @@
                     <p>&nbsp&nbsp W-ID / P-ID </p>
                     <!--Fetching Names Of All Students From Database-->
                         <?php foreach ($students as $student): ?>
-                            <li><a href="<?php echo base_url() . "index.php/zwei_update_ctrl/show_student_id/" . $student->P_ID . "/" . $student->W_ID; ?>">
+                            <li><a href="<?php echo base_url() . "index.php/zwei_update_ctrl/show_student_id/" . $student->W_ID . "/" . $student->P_ID; ?>">
 								<?php echo $student->W_ID; ?> / <?php echo $student->P_ID; ?></a></li>
                         <?php endforeach; ?>
-			<p><a href="<?php echo base_url() . "index.php/navi_ctrl/load_navi_view"; ?>">Zurück zur Navigation</a><p/>
+						<p><a href="<?php echo base_url() . "index.php/navi_ctrl/load_navi_view"; ?>">Zurück zur Navigation</a><p/>
                 </div>
                 <div id="detail">
 					<!--Fetching All Details of Selected Student From Database And Showing In a Form-->
@@ -33,8 +33,8 @@
                             <input type="submit" id="submit" name="dsubmit" value="Update">
 
 							<?php if (isset($msg_id) and ($msg_id == 1)) { ?>
-							<br/>
-							<label style="color:green;">Daten erfolgreich aktualisiert</label>
+								<br/>
+								<label style="color:green;">Daten erfolgreich aktualisiert</label>
 							<?php } ?>
 							
                             <input type="text" id="hide" name="dw_id" value="<?php echo $student->W_ID; ?>">
