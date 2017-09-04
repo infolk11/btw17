b48b06382de78fc6f06a2e438d44e2e76db2fe3b
AnonymOnline <david@anonym-online.net>
2017-09-04 17:52:50 +0200

    Merging outpatch of log4cpp in my actual development branch (#86)
    * Add files via upload
    
    WARNIG: These files are only intended for early tests. There is a major problem with Strings that have to be fixed before merging to the Master branch. Nevertheless is QDebug Logger stable for testing purposes and is able to wirte to the console and a text file.
    
    * updated logger

--- visualisierung/src/VWahl/settingswindow.cpp
+++ visualisierung/src/VWahl/settingswindow.cpp
@@ -25,7 +25,7 @@ SettingsWindow::~SettingsWindow()
  */
 void SettingsWindow::showPlot()
 {
-    Logger::log << L_INFO << "Plot changed";
+    //to do Logger::log << L_INFO << "Plot changed";
     Plots p;
     presentationWindow->showPlot(p);
 }
@@ -45,9 +45,9 @@ void SettingsWindow::init()
     if(db->connect() != EXIT_SUCCESS)
     {
         error->showMessage(db->lastError().text());
-        Logger::log << L_ERROR << db->lastError().text().toStdString();
+        //to do Logger::log << L_ERROR << db->lastError().text().toStdString();
     } else
-         Logger::log << L_INFO << "opened Database!";
+         //to do Logger::log << L_INFO << "opened Database!";
 
     dbDialog = new DatabaseDialog(this);
     queryDialog = new QueryDialog(this);
@@ -60,13 +60,14 @@ void SettingsWindow::init()
 
 void SettingsWindow::buildConnects()
 {
+
     //signal and slot in new qt5 syntax
     connect(ui->actionBeenden, &QAction::triggered,
             this, &SettingsWindow::close);
-
+/*
     connect(ui->actionDatabaseSettings, &QAction::triggered,
             this->dbDialog, &DatabaseDialog::show);
-
+*/
 
     //using lambda functions
     connect(ui->showAssociatedParty, &QCheckBox::stateChanged,
@@ -80,7 +81,7 @@ void SettingsWindow::buildConnects()
                     new QListWidgetItem(q.value(0).toString() + ", " + q.value(1).toString() + "\t" + q.value(2).toString(), ui->candidatesList);
             }
             else{
-                Logger::log << L_ERROR << q.lastError().text().toStdString();
+                //to do Logger::log << L_ERROR << q.lastError().text().toStdString();
                 error->showMessage(q.lastError().text());
                 return;
             }
@@ -93,7 +94,7 @@ void SettingsWindow::buildConnects()
                    new QListWidgetItem(q.value(0).toString() + ", " + q.value(1).toString(), ui->candidatesList);
             }
             else{
-                Logger::log << L_ERROR << q.lastError().text().toStdString();
+                //to do Logger::log << L_ERROR << q.lastError().text().toStdString();
                 error->showMessage(q.lastError().text());
                 return;
             }
@@ -120,6 +121,7 @@ void SettingsWindow::buildConnects()
 
     connect(ui->actionquerys, &QAction::triggered,
             queryDialog, &QueryDialog::show);
+
 }
 
 int SettingsWindow::fetchDatabaseValues()
@@ -146,29 +148,29 @@ int SettingsWindow::fetchDatabaseValues()
         }
     }
     else{
-        Logger::log << L_WARN << "Database connection not established, no changes made.";
+        //to do Logger::log << L_WARN << "Database connection not established, no changes made.";
         return EXIT_FAILURE;
     }
-    Logger::log << L_INFO << "The data have been succesfully fetched from the database.";
+    //to do Logger::log << L_INFO << "The data have been succesfully fetched from the database.";
     return EXIT_SUCCESS;
 }
 
 void SettingsWindow::closeEvent(QCloseEvent *event)
 {
-    Logger::log << L_DEBUG << "Close event triggered.";
+    //to do Logger::log << L_DEBUG << "Close event triggered.";
     QMessageBox msgBox;
     msgBox.setText("Möchten Sie das Programm wirklich schließen?");
     msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
     int res = msgBox.exec();
     if(QMessageBox::Yes == res)
     {
-        Logger::log << L_DEBUG << "User confirmed close event";
+        //to do Logger::log << L_DEBUG << "User confirmed close event";
         presentationWindow->close();
         event->accept();
     }
     else
     {
-        Logger::log << L_DEBUG << "User canceled close event";
+        //to do Logger::log << L_DEBUG << "User canceled close event";
         event->setAccepted(false);
     }
 }
