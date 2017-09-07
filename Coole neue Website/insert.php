c356fe1b3270e4d84f3f51ebe628ed2821b41d4d
AresCrew <Felix.schroeter@gmx.net>
2017-09-07 17:38:24 +0200

    Problembehebung
    Korrekte Befehle hinzugefügt, die die SQL-Querys auch Ausführen.

--- Coole neue Website/insert.php
+++ Coole neue Website/insert.php
@@ -1,5 +1,5 @@
 <?php
-	$servername = "localhost",
+	$servername = "localhost";
 	$username = "root";
 	$password = "";
 	$dbname= "btw_17";
@@ -14,22 +14,21 @@
 	
 	}
 	//sql parteien abfragen
-	$sql = "SELECT P_ID FROM Partei"
-	$result = $conn->exec($sql);
+	$sql = "SELECT p_id FROM partei"
+	$result = mysqli_query($conn, $sql);
 	// in result speichern
 	
 	//for each über result
 	foreach($result as $r){
-		$sql = "INSERT INTO 2stimme(W_ID,P_ID,2Anzahl) VALUES ("$wl", " + $r + ", " + $parteien[$r] + "$)";
-		$conn->exec($sql);
+		$sql = "INSERT INTO 2stimme(w_id,p_id,2anzahl) VALUES ("$wl", " + $r + ", " + $parteien[$r] + "$)";
+		$conn->mysqli_query($conn, $sql);
 	}
 		//insert wert von p[result] wo p_id: result 
 	
-	$sql = "SELECT D_ID FROM Direktkandidaten"
-	$result = $conn->exec($sql);
+	$sql = "SELECT d_id FROM direktkandidaten"
+	$result = mysqli_query($conn, $sql);
 	foreach($result as $r){
-		$sql = "INSERT INTO 1stimme(W_ID, D_ID, 1Anzahl) VALUES ("$wl", " + $r + ", " + $kandidaten[$r] + "$)";
-		$conn->exec($sql);
+		$sql = "INSERT INTO 1stimme(w_id,d_id,danzahl) VALUES ("$wl", " + $r + ", " + $kandidaten[$r] + "$)";
+		$conn->mysqli_query($conn, $sql);
 	}
 ?>
-
