ef1375b6b663f86c4bf05c0694db73905e970fd8
AresCrew <Felix.schroeter@gmx.net>
2017-09-09 16:18:48 +0200

    Fehlererkennung verbessert
    Nun kommt man auf die Fehlerbehebungsseite und bekommt seine eingegeben Daten angezeigt

--- Coole neue Website/insert.php
+++ Coole neue Website/insert.php
@@ -32,7 +32,58 @@
 	while ($r = $result->fetch_assoc()){
 		$sql = "INSERT INTO 1stimme(W_ID, D_ID, 1Anzahl) VALUES (".$wl.", ".$r["D_ID"].", ".$kandidaten[$r["D_ID"]].")";
 		mysqli_query($conn, $sql);
-	}
-	echo "Eingabe erfolgreich! :D"
-
+	} 
+	$e ="SELECT P_Bezeichnung, 2Anzahl FROM Partei P, 2stimme S WHERE
+		W_ID = '".$wl."'
+		AND
+		P.P_ID = S.P_ID;";
+	$p = mysqli_query($conn, $e);
+	$e ="SELECT Vorname, Name, 1Anzahl FROM Direktkandidaten D, 1stimme S WHERE
+		W_ID = '".$wl."'
+		AND
+		D.D_ID = S.D_ID;";
+	$d = mysqli_query($conn, $e);
+	echo "<html>
+			<head>
+			<style>
+				th, td {
+					padding: 5px;
+				}
+			</style>
+			</head>
+			<body>
+			Eingabe erfolgreich! :D <br>
+			Sie haben folgende Daten eingegeben (Bitte überprüfen sie diese nochmal):<br>
+				<table>
+				 <tr>
+					<th>Parteien</th>
+					<th>Stimmen</th>
+				 </tr>";
+	while($row = $p->fetch_assoc()) {
+			echo "<tr>
+					<td>".$row["P_Bezeichnung"]."</td>
+					<td>".$row["2Anzahl"]."</td> 
+				 </tr>
+				 ";
+		} 
+	echo "		</table> 
+				<table>
+				 <tr>
+					<th>Direktkandidaten</th>
+					<th>Stimmen</th>
+				 </tr>";
+	while($row = $d->fetch_assoc()) {
+			echo "<tr>
+					<td>".$row["Vorname"]." ".$row["Name"]."</td>
+					<td>".$row["1Anzahl"]."</td> 
+				 </tr>";
+		} 
+	
+	
+	echo ' 	</table>
+			<br>
+			 <a href="Updateauswahl.php" style="font-size:20px;">Fehler gemacht?</a> 				
+			</body>
+		<html>';
+		
 ?>
