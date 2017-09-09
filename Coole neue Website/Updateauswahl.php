2c3a50ce0f0e52a27fecf790b6967d1a1eb258d2
AresCrew <Felix.schroeter@gmx.net>
2017-09-09 16:19:20 +0200

    Fehlerbehebungsseite erstellt!

---
+++ Coole neue Website/Updateauswahl.php
@@ -0,0 +1,75 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname= "btw_17";
+
+	#$wl = $_REQUEST['wahllokal'];
+		
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) {
+		die("Connection failed".$conn->connect_error);
+	}
+	$sql = "SELECT W_Bezeichnung, W_ID FROM Wahllokal";
+	$result = mysqli_query($conn, $sql);
+	if($result->num_rows>0) {
+		echo '<!DOCTYPE html>
+				<html>
+					<!-- <meta charset="utf-8"/> -->
+					<head>
+						
+						<title> Wahllokalauswahl </title>
+					</head>
+					<body>
+						<form action = "Update.php" method="post">
+							<label for="wahllokal">Wählen sie ein Wahllokal aus:</label>
+							<select name="wahllokal" width="100px">';
+		while($row = $result->fetch_assoc()) {
+			echo "<option value = ".$row['W_ID'].">".$row["W_Bezeichnung"]."</option>";
+		}
+		echo'				</select><br><br>';
+	}else {
+		echo "Keine Wahllokale gefunden";
+	}
+		echo '				<label for="pok">Möchten sie eine Partei oder einen Kandidaten updaten?</label>
+							<select name="pok" width="100px">
+							    <option value= 1>Kandidat updaten</option>
+								<option value= 0>Partei updaten</option>
+								
+							</select><br><br>';
+	$sql = "SELECT P_Bezeichnung, P_ID FROM Partei";
+	$result = mysqli_query($conn, $sql);
+	if($result->num_rows>0) {
+		echo '		 		<label for="partei">Wählen sie eine Partei aus:</label>
+							<select name="partei" width="100px">';
+		while($row = $result->fetch_assoc()) {
+			echo "<option value = ".$row['P_ID'].">".$row["P_Bezeichnung"]."</option>";
+		}
+		echo'				</select><br><br>';
+	}else {
+		echo "Keine Parteien gefunden";
+	}
+	
+	$sql = "SELECT Vorname, Name, D_ID FROM Direktkandidaten";
+	$result = mysqli_query($conn, $sql);
+	if($result->num_rows>0) {
+		echo '		 		<label for="kandidat">Wählen sie einen Kandidaten aus:</label>
+							<select name="kandidat" width="100px">';
+		while($row = $result->fetch_assoc()) {
+			echo "<option value = ".$row['D_ID'].">".$row["Vorname"]." ".$row["Name"]."</option>";
+		}
+		echo'				</select><br><br>
+							<label for="stimmen">Stimmenanzahl:</label>
+							<input type="text" name="stimmen"><br><br>
+							<button type ="submit" name="submit">Absenden</button>
+						</form>
+					</body>
+				<html>';
+	}else {
+		echo "Keine Kandidaten gefunden";
+	}
+
+				
+	$conn->close();	
+	
+?>
---
+++ Coole neue Website/update.php
@@ -0,0 +1,89 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname= "btw_17";
+
+	$wl = $_POST['wahllokal'];
+	$stimmen = $_POST['stimmen'];
+	$partei = $_POST['partei'];
+	$kandidat = $_POST['kandidat'];
+	$pok = $_POST['pok'];
+
+	
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) {
+		die("Connection failed".$conn->connect_error);
+	
+	}
+	if ($pok == 0 ){
+		$sql = "UPDATE 2stimme
+				SET 2Anzahl ='".$stimmen."'
+				WHERE P_ID ='".$partei."' AND W_ID='".$wl."';";
+		mysqli_query($conn, $sql);
+		echo "Zweitstimme aktualisiert!<br>";
+		
+	}
+	else {
+		$sql = "UPDATE 1stimme
+				SET 1Anzahl ='".$stimmen."'
+				WHERE D_ID ='".$kandidat."' AND W_ID='".$wl."';";
+				
+		mysqli_query($conn, $sql);
+		echo "Erststimme aktualisiert!<br>";
+	}
+$e ="SELECT P_Bezeichnung, 2Anzahl FROM Partei P, 2stimme S WHERE
+		W_ID = '".$wl."'
+		AND
+		P.P_ID = S.P_ID;";
+	$p = mysqli_query($conn, $e);
+	$e ="SELECT Vorname, Name, 1Anzahl FROM Direktkandidaten D, 1stimme S WHERE
+		W_ID = '".$wl."'
+		AND
+		D.D_ID = S.D_ID;";
+	$d = mysqli_query($conn, $e);
+	echo "<html>
+			<head>
+			<style>
+				th, td {
+					padding: 5px;
+				}
+			</style>
+			</head>
+			<body>
+			Sie haben folgende Daten eingegeben (Bitte überprüfen sie diese nochmal):<br>
+				<table>
+				 <tr>
+					<th>Parteien</th>
+					<th>Stimmen</th>
+				 </tr>";
+	while($row = $p->fetch_assoc()) {
+			echo "<tr>
+					<td>".$row["P_Bezeichnung"]."</td>
+					<td>".$row["2Anzahl"]."</td> 
+				 </tr>
+				 ";
+		} 
+	echo "		</table> 
+				<table>
+				 <tr>
+					<th>Direktkandidaten</th>
+					<th>Stimmen</th>
+				 </tr>";
+	while($row = $d->fetch_assoc()) {
+			echo "<tr>
+					<td>".$row["Vorname"]." ".$row["Name"]."</td>
+					<td>".$row["1Anzahl"]."</td> 
+				 </tr>";
+		} 
+	
+	
+	echo ' 	</table>
+			<br>
+			 <a href="Updateauswahl.php" style="font-size:20px;">Fehler gemacht?</a> 				
+			</body>
+		<html>';
+		
+
+?>
+
