046669dabf2f0d7599141322c56d7056f20acbbf
Daniel Schlaugies <daniel@evi-forscht.de>
2017-09-13 21:39:22 +0200

    bei mir gehts nicht mehr :(

--- docker/remove_all.sh
+++ docker/remove_all.sh
@@ -3,3 +3,6 @@
 #stop all
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
+
+#remove custom network
+docker network rm btw17
--- docker/run_all_containers.sh
+++ docker/run_all_containers.sh
@@ -6,31 +6,34 @@ docker run -d -p 127.0.0.1:5000:5000 --restart=always --name registry registry:l
 #create bridged btw17 network
 docker network create -d bridge btw17
 
+#build own-php-apache
+docker build -t own-php-apache ./php-apache
+
 #run mysql container first
-docker run --name mysql \
-        --network btw17 \
+docker run --network btw17 \
         -d \
         --restart=unless-stopped \
+	--name mysql \
         -v mysql_datadir:/var/lib/mysql \
         -e MYSQL_ROOT_PASSWORD=btw17 \
         -p 127.0.0.1:3306:3306 \
         mysql:latest
 
 #2nd one is php-apache                                                                                                                                       
-docker run --name php-apache \                                                                                                                               
-        --network btw17 \                                                                                                                                    
+docker run --network btw17 \                                                                                                                                    
         -d \                                                                                                                                                 
         -p 80:80 \                                                                                                                                           
         --restart=unless-stopped \                                                                                                                           
+	--name phpApache \
         -v www_src:/var/www/html \                                                                                                                           
         -v php-apache_config:/usr/local/etc/php \                                                                                                            
         own-php-apache:latest                                                                                                                                
                                                                                                                                                              
 #last but not least                                                                                                                                          
-docker run --name phpmyadmin \                                                                                                                               
-        --network btw17 \                                                                                                                                    
+docker run --network btw17 \                                                                                                                                    
         -d \                                                                                                                                                 
         -p 8080:80 \                                                                                                                                         
+	--name phpmyadmin \
         -e PMA_HOST=mysql \                                                                                                                                  
         --restart=unless-stopped \                                                                                                                           
-        phpmyadmin/phpmyadmin  
+        phpmyadmin/phpmyadmin 
