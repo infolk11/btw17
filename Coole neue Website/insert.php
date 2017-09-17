0941927b92e33026826526d54b75cc6b74363330
AresCrew <Felix.schroeter@gmx.net>
2017-09-17 15:13:10 +0200

    Fixed Nullwerte, parteilos

--- Coole neue Website/insert.php
+++ Coole neue Website/insert.php
@@ -6,13 +6,17 @@
 	
 
 	$wl = $_POST['wahllokal'];
-	header ('content-type: charset=utf-8');
+	header ('content-type: text/html charset=utf-8');
 	$parteien = $_POST['partei'];
 	$kandidaten = $_POST['kandidat'];
 	
 	//sql parteien abfragen
-	$sql = "SELECT P_ID FROM Partei";
+	$sql = "SELECT P_ID, P_Bezeichnung FROM Partei";
 	$result = mysqli_query($conn, $sql);
+	//Löschen der Stimmen die schon vorhanden sind
+	$stmnt = $conn->prepare("DELETE FROM 2stimme WHERE W_ID = ?");
+	$stmnt->bind_param("i", $wl);
+	$stmnt->execute();
 	
 	$stmt = $conn->prepare("INSERT INTO 2stimme(W_ID, P_ID, 2Anzahl) VALUES (?, ?, ?)");
 	$stmt->bind_param("iii", $wl, $tmp_p_id, $tmp_parteien);
@@ -21,11 +25,24 @@
 	while ($r = $result->fetch_assoc()){
 		#Variablen aus Arrays holen, um sie in prepared Statements einzufügen
 		$tmp_p_id = $r["P_ID"];
-		$tmp_parteien = $parteien[$r["P_ID"]];
-		$stmt->execute();
+		$tmp_p_bz = $r["P_Bezeichnung"];
+		#parteilos
+		if ($tmp_p_bz=="parteilos") {
+		}
+		else{
+			$tmp_parteien = $parteien[$r["P_ID"]];
+			if ($tmp_parteien == null) {
+			}else{
+			$stmt->execute();
+			}
+		}
 	}
 	unset ($r);
-		
+	//Löschen der Stimmen die schon vorhanden sind
+	$stmnt = $conn->prepare("DELETE FROM 1stimme WHERE W_ID = ?");
+	$stmnt->bind_param("i", $wl);
+	$stmnt->execute();
+	
 	$stmt = $conn->prepare("INSERT INTO 1stimme(W_ID, D_ID, 1Anzahl) VALUES (?, ?, ?)");
 	$stmt->bind_param("iii", $wl, $tmp_d_id, $tmp_kandidaten);
 	
@@ -36,8 +53,11 @@
 		#Variablen aus Arrays holen, um sie in prepared Statements einzufügen
 		$tmp_d_id = $r["D_ID"];
 		$tmp_kandidaten = $kandidaten[$r["D_ID"]];
+		if ($tmp_kandidaten == null) {
+		}
+		else{
 		$stmt->execute();
-		
+		}
 	} 
 	$e ="SELECT P_Bezeichnung, 2Anzahl FROM Partei P, 2stimme S WHERE
 		W_ID = '".$wl."'
@@ -88,7 +108,7 @@
 	
 	echo ' 	</table>
 			<br>
-			 <a href="updateauswahl.php" style="font-size:20px;">Fehler gemacht?</a> 
+			 <a href="updateauswahl.php" style="font-size:20px;">Fehler gemacht?</a> <br> 
 			 <a href="insertauswahl.php" style="font-size:17px;">Weiter zum Daten eintragen!</a>
 			</body>
 		<html>';
@@ -99,3 +119,4 @@
 	
 	$conn->close();
 ?>
+
