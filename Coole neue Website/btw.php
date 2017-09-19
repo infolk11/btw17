f2890feeb4e0138d0e7e42a016296f2ee8877c8b
Daniel S <daniel@evi-forscht.de>
2017-09-19 18:48:38 +0200

    Update btw.php

--- Coole neue Website/btw.php
+++ Coole neue Website/btw.php
@@ -3,7 +3,7 @@
 $servername = "172.18.0.3";
 $username = "website";
 $password = "cebit17";
-$database = "_wp_BTW_DE_17";
+$database = "_wp_BTW_DE_2017";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
